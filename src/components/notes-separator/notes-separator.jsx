1e887e110e344b27c9796ca85f2cbbee8dd6fb47
vladm1r <v13911@mail.ru>
2021-10-11 16:30:32 +0300

    adaptive and styles

--- src/components/notes-separator/notes-separator.jsx
+++ src/components/notes-separator/notes-separator.jsx
@@ -2,32 +2,35 @@ import React from 'react'
 
 import NotesList from '../../components/notes-list/notes-list';
 
+import './notes-separator.scss';
+
 const NotesSeparator = ({ notesList, removeNote, changeColor, pinNote, changeText }) => {
 
 	const isNotesExist = notesList.length > 0;
 
 	if (!isNotesExist) {
 		return (
-			<div>
-				ЗАМЕТОК НЕТ
+			<div className="empty-list-container">
+				<h2 className="empty-list-title">Здесь будут заметки</h2>
 			</div>
 		)
 	}
 
-	const pinnedNotesList = notesList.filter((item) => item.isPinned);
-	notesList = notesList.filter((item) => !item.isPinned);
+	const pinnedNotesList = notesList.filter((item) => item.isPinned).reverse();
+	notesList = notesList.filter((item) => !item.isPinned).reverse();
 
 
 	return (
 		<div>
-			<NotesList
-				isPinnedExist={pinnedNotesList.length}
-				title={"Закреплённые заметки"}
-				notesList={pinnedNotesList}
-				removeNote={removeNote}
-				changeColor={changeColor}
-				pinNote={pinNote}
-				changeText={changeText} />
+			{pinnedNotesList.length > 0 &&
+				<NotesList
+					isPinnedExist={pinnedNotesList.length}
+					title={"Закреплённые"}
+					notesList={pinnedNotesList}
+					removeNote={removeNote}
+					changeColor={changeColor}
+					pinNote={pinNote}
+					changeText={changeText} />}
 
 			<NotesList
 				isPinnedExist={pinnedNotesList.length}
